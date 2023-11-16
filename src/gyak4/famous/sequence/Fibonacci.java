9ca70c0dcdc514f68a8bc37885ced075ae1f1863
rolee15 <rolee15@gmail.com>
2023-11-16 22:48:23 +0100

    Fixed fibonacci test

--- src/gyak4/famous/sequence/Fibonacci.java
+++ src/gyak4/famous/sequence/Fibonacci.java
@@ -5,32 +5,10 @@ public class Fibonacci {
     public static int fib(int n) {
         if (n <= 0) {
             return 0;
-        }
-        else if (n == 1) {
+        } else if (n == 1) {
             return 1;
-        }
-        else {
+        } else {
             return fib(n - 1) + fib(n - 2);
         }
-    }
-
-    public static int fib2(int n) {
-        if (n <= 0)
-            return 0;
-        
-        if (n < 3)
-            return 1;
-
-        int prev1 = 1;
-        int prev2 = 1;
-        int aggr = 1;
-
-        for (int i = 2; i < n; i++) {
-            aggr = prev1 + prev2;
-            prev1 = prev2;
-            prev2 = aggr;
-        }
-
-        return aggr;
     }
 }
--- src/gyak4/famous/sequence/FibonacciTest.java
+++ src/gyak4/famous/sequence/FibonacciTest.java
@@ -18,55 +18,9 @@ public class FibonacciTest {
         assertEquals(1, Fibonacci.fib(2));
     }
 
-    
+
     @Test
-    public void testFibonacci6() {
+    public void testFibonacci3() {
         assertEquals(8, Fibonacci.fib(6));
-    }
-
-
-    @Test
-    public void testFib2_0() {
-        assertEquals(0, Fibonacci.fib2(0));
-    }
-
-    @Test
-    public void testFib2_negative() {
-        assertEquals(0, Fibonacci.fib2(-1));
-    }
-
-    @Test
-    public void testFib2_1() {
-        assertEquals(1, Fibonacci.fib2(1));
-    }
-
-    @Test
-    public void testFib2_2() {
-        assertEquals(1, Fibonacci.fib2(2));
-    }
-
-    @Test
-    public void testFib2_3() {
-        assertEquals(2, Fibonacci.fib2(3));
-    }
-
-    @Test
-    public void testFib2_4() {
-        assertEquals(3, Fibonacci.fib2(4));
-    }
-
-    @Test
-    public void testFib2_5() {
-        assertEquals(5, Fibonacci.fib2(5));
-    }
-    
-    @Test
-    public void testFib2_6() {
-        assertEquals(8, Fibonacci.fib2(6));
-    }
-    
-    @Test
-    public void testFib2_7() {
-        assertEquals(13, Fibonacci.fib2(7));
     }
 }
