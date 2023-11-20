fbfda05caabfdfbac181cc8044367a93061bddcb
rolee15 <rolee15@gmail.com>
2023-11-20 18:40:54 +0100

    Moved test files to the specified subfolders

---
+++ bead/planet/test/PlanetCompulsoryTest.java
@@ -0,0 +1,11 @@
+package planet.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.junit.jupiter.params.provider.CsvSource;
+
+public class PlanetCompulsoryTest {
+
+}
